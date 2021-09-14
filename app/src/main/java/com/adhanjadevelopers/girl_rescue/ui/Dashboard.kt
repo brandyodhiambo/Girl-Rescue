0a090abe4da99a5e8d751986089a342823f41866
brandy-kay <brandyariana99@gmail.com>
2021-09-14 17:36:39 +0300

    initial commit

---
+++ app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/Dashboard.kt
@@ -0,0 +1,11 @@
+package com.adhanjadevelopers.girl_rescue.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dashboard)
+    }
+}
---
+++ app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adhanjadevelopers.girl_rescue.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adhanjadevelopers.girl_rescue.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
