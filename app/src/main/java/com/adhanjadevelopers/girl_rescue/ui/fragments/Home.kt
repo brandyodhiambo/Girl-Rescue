bbe2a0f088f0c1981cfc8975cb74897813ef015e
brandy-kay <brandyariana99@gmail.com>
2021-10-24 18:32:56 +0300

    final

--- app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/fragments/Home.kt
+++ app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/fragments/Home.kt
@@ -1,16 +1,23 @@
 package com.adhanjadevelopers.girl_rescue.ui.fragments
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.AlertDialog
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.provider.Settings
 import android.telephony.SmsManager
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import com.adhanjadevelopers.girl_rescue.database.AddGuardian
 import com.adhanjadevelopers.girl_rescue.database.GuardianDao
 import com.adhanjadevelopers.girl_rescue.database.GuardianDatabase
@@ -31,6 +38,9 @@ class Home : Fragment() {
     private lateinit var guardianDatabase: GuardianDatabase
     private lateinit var sentPendingIntent: PendingIntent
     private lateinit var deliveredPendingIntent: PendingIntent
+    private lateinit var locationManager: LocationManager
+    var latitude: String? = null
+    var longitude: String? = null
 
     val sms: SmsManager? = SmsManager.getDefault()
     override fun onCreateView(
@@ -49,13 +59,27 @@ class Home : Fragment() {
         guardianDao = guardianDatabase.guardianDao
 
         binding.helpButton.setOnClickListener {
-            displayDialog()
+
+            locationManager =
+                requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                OnGPS()
+            } else {
+                getLocationn()
+                if(latitude==null || longitude== null){
+                    return@setOnClickListener
+                }else {
+                    displayDialog(latitude!!, longitude!!)
+                }
+            }
+
         }
+
         return binding.root
     }
 
     @SuppressLint("SimpleDateFormat")
-    private fun displayDialog() {
+    private fun displayDialog(lati:String,lng:String) {
         AlertDialog.Builder(requireContext())
             .setTitle("Emergency")
             .setMessage("Are you sure you want to send this emergency message to guardians?")
@@ -68,7 +92,7 @@ class Home : Fragment() {
                         sms?.sendTextMessage(
                             details.phoneNumber,
                             null,
-                            "Hey I am in danger",
+                            "Hey I am in danger!!!"+" My location is "+"https://www.google.com/maps/search/?api=1&query=${latitude},${longitude}" ,
                             sentPendingIntent,
                             deliveredPendingIntent
                         )
@@ -79,6 +103,48 @@ class Home : Fragment() {
             .setNegativeButton("No", null)
             .show()
     }
+    private fun getLocationn(){
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }else{
+            val locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (locationGPS != null) {
+                val lat = locationGPS.latitude
+                val longi = locationGPS.longitude
+
+                latitude = lat.toString()
+                longitude = longi.toString()
+
+                Toast.makeText(requireContext(), "found location ${latitude} ${longitude}", Toast.LENGTH_SHORT)
+                    .show()
+            } else {
+                Toast.makeText(requireContext(), "Unable to find location", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+
+    private fun OnGPS() {
+        AlertDialog.Builder(requireContext())
+            .setMessage("Enable GPS ")
+            .setCancelable(false)
+            .setPositiveButton(
+                "yes"
+            ) { dialog, which -> startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)) }
+            .setNegativeButton(
+                "No"
+            ) { dialog, which -> dialog?.cancel() }
+            .show()
+
+    }
+
 
 
 
--- app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/fragments/MapsFragment.kt
+++ app/src/main/java/com/adhanjadevelopers/girl_rescue/ui/fragments/MapsFragment.kt
@@ -1,12 +1,19 @@
 package com.adhanjadevelopers.girl_rescue.ui.fragments
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.pm.PackageManager
 import androidx.fragment.app.Fragment
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.adhanjadevelopers.girl_rescue.R
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -15,29 +22,65 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 
-class MapsFragment : Fragment() {
+import android.content.Context
+import android.location.LocationManager
+import com.adhanjadevelopers.girl_rescue.R
+import androidx.constraintlayout.motion.widget.Debug.getLocation
 
+import androidx.core.content.ContextCompat.getSystemService
+import android.content.DialogInterface
+
+import android.content.Intent
+import android.location.Location
+import android.media.audiofx.EnvironmentalReverb
+import android.provider.Settings
+import android.util.Log
+import android.widget.Toast
+
+private const val TAG = "MapsFragment"
+
+class MapsFragment : Fragment() {
+    private lateinit var map: GoogleMap
+    private val REQUEST_LOCATION_PERMISSION = 1
     private val callback = OnMapReadyCallback { googleMap ->
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        val sydney = LatLng(-34.0, 151.0)
-        googleMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        map = googleMap
+
+/*
+    *
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera.
+     * In this case, we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to
+     * install it inside the SupportMapFragment. This method will only be triggered once the
+     * user has installed Google Play services and returned to the app.
+*/
+
+
+        val latitude = 0.570044
+        val longitude = 34.559244
+        val zoomLevel = 18f
+        val homeLatLng = LatLng(latitude, longitude)
+
+        googleMap.addMarker(MarkerOptions().position(homeLatLng))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(homeLatLng, zoomLevel))
+        enableMyLocation()
     }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
+
+        val view = inflater.inflate(
+            R.layout.fragment_maps,
+            container,
+            false
+        )
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,4 +88,50 @@ class MapsFragment : Fragment() {
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
     }
+
+    private fun isPermissionGranted(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            requireContext(), ACCESS_FINE_LOCATION
+        ) === PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun enableMyLocation() {
+        if (isPermissionGranted()) {
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+
+                return
+            }
+            map.isMyLocationEnabled = true
+
+        } else {
+            ActivityCompat.requestPermissions(
+                requireContext() as Activity, arrayOf(ACCESS_FINE_LOCATION),
+                REQUEST_LOCATION_PERMISSION
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.size > 0 && (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                enableMyLocation()
+        }
+    }
+
+
+
+
 }
